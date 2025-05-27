87be63fcb2fceaf6fcca502c97262d216e0a0841
Marcosdev47 <mvpetrolina@hotmail.com>
2025-05-27 14:05:27 +0000

    Exibindo os valores das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -82,11 +82,27 @@ int main() {
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pontosTur2);
 
+    // Exibição da carta 1
 
+    printf("\nCarta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nomeCidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n\n", pontosTur1);
 
+    // Exibição da Carta 2
 
-
-    printf("Ola Mundo!");
+    printf("Carta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nomeCidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontosTur2);
     
     
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
