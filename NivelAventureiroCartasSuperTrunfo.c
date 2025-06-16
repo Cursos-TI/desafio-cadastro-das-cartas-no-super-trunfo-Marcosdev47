8f53bdda34e4171ad8a69ac298c759761a9a8201
Marcosdev47 <mvpetrolina@hotmail.com>
2025-06-16 23:04:25 +0000

    Declarando as variáveis para a carta 1

--- NivelAventureiroCartasSuperTrunfo.c
+++ NivelAventureiroCartasSuperTrunfo.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-// Função para limpar o buffer do teclado
-void limparBuffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-}
-
 int main() {
     // Declaração de variáveis para a Carta 1
+
     char estado1;
     char codigo1[4];
     char nomeCidade1[50];
@@ -18,15 +13,3 @@ int main() {
     int pontosTuristicos1;
     float densidade1;
     float pibPerCapita1;
-
-   // Declaração de variáveis para a Carta 2
-    char estado2;
-    char codigo2[4];
-    char nomeCidade2[50];
-    int populacao2;
-    float area2;
-    double pib2;
-    int pontosTuristicos2;
-    float densidade2;
-    float pibPerCapita2;
-
