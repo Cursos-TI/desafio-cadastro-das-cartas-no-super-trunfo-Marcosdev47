b4811d864dda0f12719e51a5f02c03c1debbcc19
Marcosdev47 <mvpetrolina@hotmail.com>
2025-06-18 00:14:45 +0000

    Declarando as variáveis das cartas 1 e 2

---
+++ NivelMestreCartasSuperTrunfo.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    // Declaração de variáveis para a Carta 1
+    char estado1;
+    char codigo1[4];
+    char nomeCidade1[50];
+    unsigned long int populacao1;
+    float area1;
+    double pib1;
+    int pontosTuristicos1;
+    float densidade1;
+    float pibPerCapita1;
+    float superPoder1;
+
+    // Declaração de variáveis para a Carta 2
+    char estado2;
+    char codigo2[4];
+    char nomeCidade2[50];
+    unsigned long int populacao2;
+    float area2;
+    double pib2;
+    int pontosTuristicos2;
+    float densidade2;
+    float pibPerCapita2;
+    float superPoder2;
+
+    
