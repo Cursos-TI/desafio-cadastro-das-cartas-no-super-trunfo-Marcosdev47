e597fa82218eb73d8de5ce5ddae53a5fe826d622
Marcosdev47 <mvpetrolina@hotmail.com>
2025-06-18 00:22:44 +0000

    Exibindo os dados de cada carta

--- NivelMestreCartasSuperTrunfo.c
+++ NivelMestreCartasSuperTrunfo.c
@@ -74,3 +74,30 @@ int main() {
     densidade2 = populacao2 / area2;
     pibPerCapita2 = (pib2 * 1e9) / populacao2;
     superPoder2 = (float)populacao2 + area2 + (float)(pib2 * 1e9) + pontosTuristicos2 + pibPerCapita2 + (1.0f / densidade2);
+
+    // Exibição final dos dados
+    printf("\nDados das Cartas\n");
+
+    printf("\nCarta 1:\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nomeCidade1);
+    printf("População: %lu\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
+    printf("Super Poder: %.2f\n", superPoder1);
+
+    printf("\nCarta 2:\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nomeCidade2);
+    printf("População: %lu\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
+    printf("Super Poder: %.2f\n", superPoder2);
